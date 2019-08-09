8022d0e1b5687558477b93b168893503cc73c1dd
zhiwen <zhiwen@kooboo.cn>
2019-08-09 23:31:11 +0800

    IdentityServer4 quick start

---
+++ IdentityServer4.Client/Config.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IdentityServer4.Models;
+
+namespace IdentityServer4.Client
+{
+    public static class Config
+    {
+        public static IEnumerable<Models.Client> GetClients()
+        {
+            return new List<Models.Client>
+            {
+                new Models.Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
